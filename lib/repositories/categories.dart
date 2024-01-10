7a3ade3a62a345ad2e139da2c30535c27de300dd
EzequielPires <ezequiel.pires082000@gmail.com>
2024-01-10 16:15:34 -0300

    Commit

--- lib/repositories/categories.dart
+++ lib/repositories/categories.dart
@@ -4,13 +4,13 @@ import 'package:anhangueraonline/models/document_category.dart';
 List<Category> categories = [
   Category(name: 'Denúncia Lotes Sujos', image: 'images/trator.png', id: 1),
   Category(name: 'Obstrução de Calçada', image: 'images/calcada.png', id: 2),
-  Category(name: 'Obra Irregular', image: 'images/obrainregular.png', id: 3),
-  Category(name: 'Trânsito Semáforos', image: 'images/transito.png', id: 4),
+  Category(name: 'Solicitação de Benefícios Sociais', image: 'images/social.png', id: 3),
+  Category(name: 'Solicitação de Caçamba', image: 'images/cacamba.png', id: 4),
   Category(name: 'Iluminação Pública', image: 'images/luz.png', id: 5),
   Category(name: 'Tapa Buracos', image: 'images/tapaburaco.png', id: 6),
   Category(name: 'Limpeza de Bueiros', image: 'images/bueiro.png', id: 7),
   Category(name: 'Roçagem', image: 'images/rocagem.png', id: 8),
-  Category(name: 'Água Servida e Criação de Animais', image: 'images/animals.png', id: 9),
+  Category(name: 'Mataburros e Estradas Rurais', image: 'images/animais.png', id: 9),
 ];
 
 List<DocumentCategory> documentCategories = [
